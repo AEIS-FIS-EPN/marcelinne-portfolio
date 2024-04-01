a3d35683aebf16b0c668c52053d73d6a2cf40ffa
marcelinne <marcela.montalvo@epn.edu.ec>
2024-04-01 08:51:25 -0500

    feat: Install and configure Tailwind CSS
    Add Tailwind CSS to the project dependencies and configure it in the  file. Ensure correct setup and integration of Tailwind CSS into the project.

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="app" className="relative h-screen p-2 gap-2">
+      <header className="[grid-area:aside] flex-col flex overflow-y-auto">
+        <h1> Hola</h1>
+      </header>
+
+      <main
+        className="[grid-area:main] rounded-lg bg-zinc-900 overflow-y-auto w-full"
+      >
+        <h2>Marcelinne</h2>
+      </main>
+
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold text-center bg-blue-100">Using Tailwind</h1>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <footer className="[grid-area:player] h-[80px]">
+        <h3>Footer 2</h3>
+      </footer>
+    </div>
     </>
   )
 }
