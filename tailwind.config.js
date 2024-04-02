be768fe20d5c7cba8ee58a02e89d7e5feb13da98
marcelinne <marcela.montalvo@epn.edu.ec>
2024-04-01 23:12:18 -0500

    feat: add cover profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'];
+export const content = ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}']
 export const theme = {
-  extend: {},
-};
-export const plugins = [];
+  extend: {}
+}
+export const plugins = []
